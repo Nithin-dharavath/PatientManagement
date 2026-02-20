27f8cc4c15d06ff9038fdc222340fab367a575ab
nithin-dharavathu <nithindharavth01@gmail.com>
2026-02-20 16:04:30 +0530

    path and Query code Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -23,9 +23,24 @@ def view_data():
     return data
 
 @app.get("/patient/{patient_id}")
-def Patient_id(patient_id : str):
+def Patient_id(patient_id : str = Path(..., description = " id od patient database", example="P001")):
     data = load_data()
     if patient_id in data:
         return data[patient_id]
-    else:
-        return {"error" : "patient details not found"}
+    raise HTTPException(status_code = 404, detail = "patient not found")
+
+@app.get("/sort")
+def sorted_patient(sort_by : str = Query(..., description="sort on bias of height, weight or bmi"), order : str = Query("asc", description="sort by asc or desc order")):
+    
+    valid_fields = ["height", "weight", "bmi"]
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code="404", detail="enter the vaild choice from {valid_fields}")
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=404, detail="invalid order")
+    
+    data = load_data()
+    sort_data = True if order == "desc" else False
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by, 0), reverse=sort_data)
+
+    return sorted_data
